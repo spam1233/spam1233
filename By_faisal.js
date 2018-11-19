02d178a9863da33786b4cb4b03ef2fdd6ce96fe1
spam1233 <45171385+spam1233@users.noreply.github.com>
2018-11-19 15:49:18 +0200

    Add files via upload

---
+++ By_faisal.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("ايدي الروم")
+setInterval(function() {
+channel.send(`اسم الروم `);
+}, 25)
+})
+ 
+ 
+client.login('TOKEN');
